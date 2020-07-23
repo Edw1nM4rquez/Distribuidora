935237c36e8907801a374e175024d530a6e104ed
Edw1nM4rquez <edwi1999@hotmail.com>
2020-07-23 10:00:16 -0500

    Borrar css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ProductolistaComponent} from './components/productolista/productolista.component';
 import { IniciarSesionComponent } from './clientes/iniciar-sesion/iniciar-sesion.component';
+import { ModificarComponent } from './clientes/modificar/modificar.component';
+import { RegistrarComponent } from './clientes/registrar/registrar.component';
 
 const routes: Routes = [
-  {path: '', redirectTo:'/Producto',pathMatch:'full' },
+  {path: '', redirectTo:'/home',pathMatch:'full' },
   {path: 'Producto', component: ProductolistaComponent},
-  {path: 'Login', component: IniciarSesionComponent}
+  {path: 'Login', component: IniciarSesionComponent},
+  {path: 'Modificar', component: ModificarComponent},
+  {path: 'Registrar', component:RegistrarComponent }
+ 
 ];
 
 @NgModule({
